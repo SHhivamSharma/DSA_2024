8956694df33a4504b2dd3ef2bc4b9d76432a3d0c
ShivamSharmaDev21 <151991211+ShivamSharmaDev21@users.noreply.github.com>
2024-05-18 14:31:11 +0530

    testing 1

---
+++ Questions/Pattern Problems/HelloWorld.java
@@ -0,0 +1,11 @@
+// import java.util.*;
+
+public class HelloWorld {
+    // public static void main( String[] args ) {
+    //     System.out.println("someVariable");
+    // }
+
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+    }
+}
