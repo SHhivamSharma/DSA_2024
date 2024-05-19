f0f78510da4e0fbe0dee258d06148578018003bb
ShivamSharmaDev21 <151991211+ShivamSharmaDev21@users.noreply.github.com>
2024-05-19 05:55:09 +0530

    Pattern04

--- Patterns/Pattern/src/Pattern1.java
+++ Patterns/Pattern/src/Pattern1.java
@@ -8,7 +8,8 @@ public class Pattern1 {
         // Pattern01(n);
         // Pattern02(n);
         // Pattern03(n);
-        Pattern03_1(n);
+        // Pattern03_1(n);
+        Pattern04(n);
 
     }
 
@@ -75,13 +76,6 @@ public class Pattern1 {
     */
 
     public static void Pattern03_1(int n){
-        // for(int i = 0 ; i<n ; i++){
-        //     for(int j = 0 ; j<n-i ; j++){
-        //         System.out.print("*\t");
-        //     }
-        //     System.out.println();
-        // }
-
         for(int i = n ; i>=1 ; i--){
             for(int j = i ; j>=1 ; j--){
                 System.out.print("*\t");
@@ -89,4 +83,28 @@ public class Pattern1 {
             System.out.println();
         }
     }
+
+
+    /* for n = 5 By revesing the loop 
+    1
+    1       2
+    1       2       3
+    1       2       3       4
+    1       2       3       4       5
+    */
+
+    public static void Pattern04(int n){
+        int Number = 1;
+
+        for(int i = 0 ; i<n; i++){
+            for(int j = 0 ; j<=i ;j++){
+                System.out.print(Number+"\t");
+                Number++;
+            }
+            System.out.println();
+            Number = 1;
+        }
+
+    }
+
 }
