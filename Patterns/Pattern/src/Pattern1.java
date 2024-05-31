45e71dcf7555f13e81df326ca9b20472b7e6e9af
ShivamSharmaDev21 <151991211+ShivamSharmaDev21@users.noreply.github.com>
2024-06-01 01:53:04 +0530

    Pattern21
    Pattern21

--- Patterns/Pattern/src/Pattern1.java
+++ Patterns/Pattern/src/Pattern1.java
@@ -568,6 +568,18 @@ public class Pattern1 {
     }
 
 
+    /* for n = 5 
+        *        *
+        **      **
+        ***    ***
+        ****  ****
+        **********
+        ****  ****
+        ***    ***
+        **      **
+        *        *
+    */
+
     public static void Pattern20(int n ){
         int nst = 0 ;
         int nsp = (2*n) ;
@@ -595,13 +607,36 @@ public class Pattern1 {
 
     }
 
+
+
+    /* for n = 5 
+        *****
+        *   *
+        *   *
+        *   *
+        *****
+    */
+    public static void Pattern21(int n ){
+
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if( i == 0 || j == 0 || i == (n-1 )|| j == (n-1) ){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
     /// Main function 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter Number");
 
         int n = scn.nextInt() ; 
-        Pattern20(n);
+        Pattern21(n);
     }
 
 
