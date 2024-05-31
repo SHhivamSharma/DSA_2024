7ca43d63e33b1f4a58818633816ab1293fa7f5aa
ShivamSharmaDev21 <151991211+ShivamSharmaDev21@users.noreply.github.com>
2024-06-01 01:12:23 +0530

    Pattern19
    Pattern19

--- Patterns/Pattern/src/Pattern1.java
+++ Patterns/Pattern/src/Pattern1.java
@@ -519,13 +519,61 @@ public class Pattern1 {
     }
 
 
+    /* for n = 5 
+        **********
+        ****  ****
+        ***    ***
+        **      **
+        *        *
+        *        *
+        **      **
+        ***    ***
+        ****  ****
+        **********
+     */
+
+     public static void Pattern19(int n ){
+
+        for(int i = 0 ; i< n; i++){
+            for(int j = 0 ; j < n-i ; j++){
+                System.out.print("*");
+            }
+
+            for(int j = 0 ; j < 2*i ; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0 ; j < n-i ; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for(int i = 1 ; i<= n; i++) {
+            for(int j = 0 ; j < i ; j++){
+                System.out.print("*");
+            }
+
+            for(int j = 0 ; j < (2*n) - (2*i) ; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 0 ; j < i ; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+
     /// Main function 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter Number");
 
         int n = scn.nextInt() ; 
-        Pattern18(n);
+        Pattern19(n);
     }
 
 
