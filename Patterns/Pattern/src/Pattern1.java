4bd444f4cfbbd57af830fb2585fa70a5d94f95a2
ShivamSharmaDev21 <151991211+ShivamSharmaDev21@users.noreply.github.com>
2024-06-01 18:59:52 +0530

    Pattern22
    Pattern22

--- Patterns/Pattern/src/Pattern1.java
+++ Patterns/Pattern/src/Pattern1.java
@@ -630,13 +630,42 @@ public class Pattern1 {
         }
 
     }
+
+
+    /* for n = 4
+  
+    4444444
+    4333334
+    4322234
+    4321234
+    4322234
+    4333334
+    4444444
+    */
+    public static void Pattern22(int n ){
+
+      for(int i = 0 ; i< (2*n)-1 ; i++)
+        {
+            for(int j =0 ; j < (2*n)-1 ; j++){
+                int top = i;
+                int left = j ;
+                int right = (2*n -1)-1 -j;
+                int bottom = (2*n -1)-1 -i;
+                int value = Math.min(Math.min(right, bottom) , Math.min (top ,left) );
+                System.out.print(n-value);
+            }
+            System.out.println();
+
+        }
+    }
+
     /// Main function 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter Number");
 
         int n = scn.nextInt() ; 
-        Pattern21(n);
+        Pattern22(n);
     }
 
 
