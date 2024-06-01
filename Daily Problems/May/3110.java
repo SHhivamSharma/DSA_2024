951a5278f1520295c7cd31cff4edc202bcc1abb8
ShivamSharmaDev21 <151991211+ShivamSharmaDev21@users.noreply.github.com>
2024-06-02 00:39:25 +0530

    leetcode Daily problem
    leetcode Daily problem

---
+++ Daily Problems/May/3110.java
@@ -0,0 +1,12 @@
+// 3110. Score of a String leetcode Daily problem
+
+
+class Solution {
+    public int scoreOfString(String s) {
+        int total = 0;
+        for(int i = 0; i < s.length() - 1; i++) {
+            total += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return total;
+    }
+}
