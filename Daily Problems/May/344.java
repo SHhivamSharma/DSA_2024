0a65dbb5ea4fa57e712303e5615580b06d2f4ccf
ShivamSharmaDev21 <151991211+ShivamSharmaDev21@users.noreply.github.com>
2024-06-03 00:24:05 +0530

    leetcode Daily problem
     leetcode Daily problem

---
+++ Daily Problems/May/344.java
@@ -0,0 +1,14 @@
+// 344. Reverse String  leetcode Daily problem
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
