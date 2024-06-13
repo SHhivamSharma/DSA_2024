5cd4af39fa442f1cf01676c7db00fa7dd20b4c93
ShivamSharmaDev21 <shivamsharma9868789@gmail.com>
2024-06-13 21:40:39 +0530

    Leetcodde

---
+++ Daily Problems/2037.java
@@ -0,0 +1,13 @@
+///2037. Minimum Number of Moves to Seat Everyone
+
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int ans = 0;
+        for (int i = 0; i < seats.length; i++) {
+            ans += Math.abs(seats[i] - students[i]);
+        }
+        return ans;
+    }
+}   
