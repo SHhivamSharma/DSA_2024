c77e3ba02c4e3de3f6c036b21550135d075239ff
ShivamSharmaDev21 <shivamsharma9868789@gmail.com>
2024-06-18 00:45:48 +0530

    Update 1051.java

--- Daily Problems/May/1051.java
+++ Daily Problems/May/1051.java
@@ -17,3 +17,19 @@ class Solution {
         return ans;
     }
 }
+
+
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] sortedHeights = heights.clone();
+        Arrays.sort(sortedHeights);
+        int count = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != sortedHeights[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
