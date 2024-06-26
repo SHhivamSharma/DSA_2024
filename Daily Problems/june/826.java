f0dff916f5d387b174482a692c1418a567e197b9
SHhivamSharma <shivamsharma9868789@gmail.com>
2024-06-27 00:52:45 +0530

    826 sol 2
    826 sol 2

--- Daily Problems/june/826.java
+++ Daily Problems/june/826.java
@@ -1,5 +1,31 @@
 //826. Most Profit Assigning Work
 
+
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int res = 0 , k = 0 , tempBest = 0 ; 
+        ArrayList<Pair<Integer , Integer>> temp = new ArrayList<>();
+        for(int i = 0 ; i < difficulty.length ; i++){
+            temp.add(new Pair<>(difficulty[i] ,profit[i] ));
+        }
+        temp.sort((m,n) -> m.getKey() - n.getKey());
+        Arrays.sort(worker);
+
+        for(int j = 0 ; j < worker.length ; j++){
+            while(k < temp.size() && worker[j] >= temp.get(k).getKey() ){
+                tempBest = Math.max(tempBest , temp.get(k).getValue() );
+                k++;
+            }
+            res+=tempBest;
+        }
+        return res;
+    }
+}
+
+
+---------------------- ---------------------- ----------------------
+ 
+
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         int res = 0, j = 0, best = 0;
